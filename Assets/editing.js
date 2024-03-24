e0a39dd1a50541109e7c97ddc4bdd7781b8e6f70
a1creator <asbjorn15johnsen@gmail.com>
2024-03-24 22:50:45 +0100

    pattern upload and selection works

--- Assets/editing.js
+++ Assets/editing.js
@@ -9,15 +9,93 @@ let editingContainer = document.getElementById("resume-editing-container");
 
 
 /* IMAGE PICKER */
+let patternCirclesParent = document.getElementById("pattern-circles-container");
+
+let selectedPCircle = null;
+
 let patternDataObjs = [
     {
+        filename:"bckgrnd_pattern_00.png",
+        setPattern:(element, imgurl) => { selectPattern( element, imgurl ); },
+        element:null,
+    },
+    {
+        filename:"bckgrnd_pattern_01.png",
+        setPattern:(element, imgurl) => { selectPattern( element, imgurl ); },
+        element:null,
+    },
+    {
         filename:"bckgrnd_pattern_02.png",
-        setPattern:()=>{  },
+        setPattern:(element, imgurl) => { selectPattern( element, imgurl ); },
+        element:null,
+    },
+    {
+        filename:"bckgrnd_pattern_03.png",
+        setPattern:(element, imgurl) => { selectPattern( element, imgurl ); },
+        element:null,
+    },
+    {
+        filename:"icon_empty.png",
+        setPattern:(element, imgurl) => { selectPattern( element, "none" ); },
+        element:null,
     },
 ]
 
+function selectPattern(element, imgurl)
+{
+    document.documentElement.style.setProperty(`--background-pattern`, imgurl)
+    // show the selected one
+    if (selectedPCircle)
+    {
+        selectedPCircle.classList.remove("selected-pattern-circle");
+    }
+    element.classList.add("selected-pattern-circle");
+    selectedPCircle = element;
+}
 
+function addPatternSeletor(patternDataObj, styleURL)
+{
+    let patternSelector = document.createElement("div");
+    patternSelector.className = "circle-option pattern-selector";
+    patternSelector.style.backgroundImage = styleURL;
+    patternSelector.onclick = () => {
+        patternDataObj.setPattern(patternSelector, `url('${patternDataObj.filename}')`);
+    };
 
+    patternDataObj.element = patternSelector;
+    patternCirclesParent.prepend(patternSelector);
+}
+
+for (let i = 0; i < patternDataObjs.length; i++) {
+    addPatternSeletor(patternDataObjs[i],
+                      `url('Assets/${patternDataObjs[i].filename}')`);
+}
+// mark the default pattern as selected
+selectPattern(patternDataObjs[2].element);
+
+// uploading
+let patternUploadButton = document.getElementById("edit-image-picker");
+patternUploadButton.oninput = (e) => {
+    
+    var newPatternObj = {
+        filename:"patternUploadButton.result",
+        setPattern:(element, imgurl) => { selectPattern( element, imgurl ); },
+    }
+
+    var files = patternUploadButton.files[0];
+
+    if (files) {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(files);
+        fileReader.addEventListener("load", function () {
+            newPatternObj.filename = this.result;
+            // add it after its loaded, otherwise it will be fucked
+            addPatternSeletor(newPatternObj, `url('${newPatternObj.filename}')`);
+        });    
+    }
+    
+    patternDataObjs.push(newPatternObj);
+};
 
 
 /* COLOUR PICKER */
@@ -41,7 +119,7 @@ let sliderDataObjs = [
     {
         name:"infsize",
         info:"Information",
-        sliderProperties:{min:0.125, max:3,step:0.125, defVal:1.25, unit:"rem"},
+        sliderProperties:{min:0.125, max:3,step:0.125, defVal:1.375, unit:"rem"},
         changeSizeProperty:(value)=>{document.documentElement.style.setProperty(`--fontsize-information`, `${value}rem`)},
     },
     {
@@ -72,8 +150,8 @@ for (let i = 0; i < sliderDataObjs.length; i++) {
     inputSlider.type = "range";
     inputSlider.min = sliderDataObjs[i].sliderProperties.min;
     inputSlider.max = sliderDataObjs[i].sliderProperties.max;
-    inputSlider.defaultValue = sliderDataObjs[i].sliderProperties.defVal;
     inputSlider.step = sliderDataObjs[i].sliderProperties.step;
+    inputSlider.value = sliderDataObjs[i].sliderProperties.defVal;
     inputSlider.oninput = (e) => {
         console.log(inputSlider.value);
         title.innerText = `${sliderDataObjs[i].info} font-size: ${inputSlider.value}${sliderDataObjs[i].sliderProperties.unit}`;
