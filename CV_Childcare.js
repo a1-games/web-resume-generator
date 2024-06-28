1bc6fff7c50a2501e09304d9b9d260b1e49b6880
a1-creator <asbjorn15johnsen@gmail.com>
2024-06-28 23:55:07 +0200

    print fix

---
+++ CV_Childcare.js
@@ -0,0 +1,117 @@
+
+
+
+
+// headline
+
+setHeadlineName("Asbjørn Ørts Johnsen");
+
+setOnelinerText("Erfaren og ansvarsfuld børne-\nog ungdomsmedarbejder");
+
+
+
+// bullet point information
+
+
+
+insertSeperationLine(shortinfContainer);
+
+addLineToShortinfoArray("phone", "50 46 38 28", true);
+addLineToShortinfoArray("location", "Brabrand, Danmark", true);
+addLineToShortinfoArray("email", "asbjorn15johnsen@gmail.com", true);
+appendShortInfo();
+
+insertInformationTitle("pædagogiske evner", shortinfContainer);
+
+insertSeperationLine(shortinfContainer);
+
+addLineToShortinfoArray("checkmark", "Tålmodig", true);
+addLineToShortinfoArray("checkmark", "Løsningsorienteret", true);
+addLineToShortinfoArray("checkmark", "Pålidelig", true);
+addLineToShortinfoArray("checkmark", "Ansvarsbevidst", true);
+addLineToShortinfoArray("checkmark", "Empatisk", true);
+addLineToShortinfoArray("checkmark", "Legeglad", true);
+appendShortInfo();
+
+insertInformationTitle("nyttige evner", shortinfContainer);
+
+insertSeperationLine(shortinfContainer);
+
+addLineToShortinfoArray("checkmark", "Kreativ", true);
+addLineToShortinfoArray("checkmark", "Spejder", true);
+addLineToShortinfoArray("checkmark", "Musikalsk", true);
+appendShortInfo();
+
+
+insertInformationTitle("Sprog", shortinfContainer);
+
+insertSeperationLine(shortinfContainer);
+
+addLineToShortinfoArray("flag_dk", "Modersmål", false);
+addLineToShortinfoArray("flag_eng", "Flydende", false);
+appendShortInfo();
+
+
+
+// profile
+insertInformationTitle("Profil", infContainer);
+
+insertSeperationLine(infContainer);
+
+insertText("Jeg er en engageret og ansvarlig person med erfaring som frivillig leder, lærervikar og au pair. Gennem disse roller har jeg udviklet mine evner til at planlægge og gennemføre kreative og udviklende aktiviteter for børn i forskellige aldersgrupper. Jeg har også erfaring med børn med særlige behov, vaner og adfærdsmønstre.", infContainer)
+
+// text information
+
+insertInformationTitle("Arbejdserfaring", infContainer);
+
+insertSeperationLine(infContainer);
+
+insertInformation("Jun 2018 - Dec 2019",
+                    "FDF Rødding",
+                    "Frivillig Leder",
+                    "Var frivillig FDF-leder i Rødding, hvor vi fandt på lege og aktiviteter til halvanden time hver uge for børn i alderen 13-15.");
+
+insertInformation("Sep 2019 - Dec 2019",
+                    "Rødding Friskole",
+                    "Lærervikar",
+                    "Underviste fra 0. klasse op til 9. klasse i dansk, engelsk, matematik, idræt og madkundskab. Jeg stoppede for at tage til Canada.");
+
+insertInformation("Jan 2020 - July 2020",
+                    "Canadisk Familie",
+                    "Au Pair",
+                    "Passede 4 børn på 6, 8, 11 og 13 i Yukon, Canada i en periode på 6 måneder.");
+
+                    
+insertInformationTitle("Anden erfaring", infContainer);
+
+insertSeperationLine(infContainer);
+
+insertInformation("2011 >>",
+                    "Hjemme",
+                    "Plejestorebror til børn med autisme og ADHD",
+                    "Jeg har søskende med særlige behøv, hvilket gør at jeg er god til at håndtere børn som har svært ved at holde opmærksomheden eller er berøringsangst og/eller indelukkede.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
